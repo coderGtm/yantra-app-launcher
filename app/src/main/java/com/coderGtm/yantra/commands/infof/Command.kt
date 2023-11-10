148f71720d1c223b3391c72a27d52b9453ef0299
coderGtm <gautammehta0000@gmail.com>
2023-11-10 11:27:33 +0530

    fixed infof command

--- app/src/main/java/com/coderGtm/yantra/commands/infof/Command.kt
+++ app/src/main/java/com/coderGtm/yantra/commands/infof/Command.kt
@@ -5,6 +5,7 @@ import android.net.Uri
 import android.provider.Settings
 import com.coderGtm.yantra.R
 import com.coderGtm.yantra.blueprints.BaseCommand
+import com.coderGtm.yantra.findSimilarity
 import com.coderGtm.yantra.models.AppBlock
 import com.coderGtm.yantra.models.CommandMetadata
 import com.coderGtm.yantra.terminal.Terminal
@@ -19,42 +20,17 @@ class Command(terminal: Terminal) : BaseCommand(terminal) {
     override fun execute(command: String) {
         val args = command.split(" ")
         val name = command.removePrefix(args[0]).trim().lowercase()
-        val candidates = mutableListOf<AppBlock>()
-        //wait till appList has been initialized
+        val candidates = mutableListOf<Double>()
         for (app in terminal.appList) {
-            if (app.appName.lowercase() == name) {
-                candidates.add(app)
-            }
+            val score = findSimilarity(app.appName.lowercase(), name)
+            candidates.add(score)
+            //addToPrevTxt(app.appName+" ---> "+score.toString(),4)
         }
-        if (candidates.size == 1) {
-            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-            intent.data = Uri.parse("package:"+candidates[0].packageName)
-            terminal.activity.startActivity(intent)
-            output("Opened settings for ${candidates[0].appName}", terminal.theme.successTextColor)
-        }
-        else if (candidates.size > 1) {
-            val b1 = MaterialAlertDialogBuilder(terminal.activity, R.style.Theme_AlertDialog)
-                .setTitle("Multiple apps found")
-                .setMessage("Multiple apps found with name '$name'. Please select one.")
-                .setPositiveButton("OK") { _, _ ->
-                    val items = mutableListOf<String>()
-                    for (app in candidates) {
-                        items.add(app.packageName)
-                    }
-                    val b2 = MaterialAlertDialogBuilder(terminal.activity, R.style.Theme_AlertDialog)
-                        .setTitle("Select Package Name")
-                        .setItems(items.toTypedArray()) { _, which ->
-                            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-                            intent.data = Uri.parse("package:"+candidates[which].packageName)
-                            terminal.activity.startActivity(intent)
-                            output("Opened settings for ${candidates[which].appName}", terminal.theme.successTextColor)
-                        }
-                    terminal.activity.runOnUiThread { b2.show() }
-                }
-            terminal.activity.runOnUiThread { b1.show() }
-        }
-        else {
-            output("'$name' app not found. Try using 'list apps' to get list of all app names.", terminal.theme.warningTextColor)
-        }
+        val maxIndex = candidates.indexOf(candidates.max())
+        val appBlock = terminal.appList[maxIndex]
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+        intent.data = Uri.parse("package:"+appBlock.packageName)
+        terminal.activity.startActivity(intent)
+        output("Opened settings for ${terminal.appList[maxIndex].appName}", terminal.theme.successTextColor)
     }
 }
