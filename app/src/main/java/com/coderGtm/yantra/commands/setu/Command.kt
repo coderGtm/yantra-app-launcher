1eb73c0793c1e8a4349d38755eb9f039bb2a47a5
coderGtm <gautammehta0000@gmail.com>
2024-01-06 15:36:43 +0530

    setu base

---
+++ app/src/main/java/com/coderGtm/yantra/commands/setu/Command.kt
@@ -0,0 +1,18 @@
+package com.coderGtm.yantra.commands.setu
+
+import com.coderGtm.yantra.blueprints.BaseCommand
+import com.coderGtm.yantra.models.CommandMetadata
+import com.coderGtm.yantra.terminal.Terminal
+
+class Command(terminal: Terminal) : BaseCommand(terminal) {
+    override val metadata = CommandMetadata(
+        name = "setu",
+        helpTitle = "setu [new][name]",
+        description = "Setu is used to form Custom Bridges between Yantra Launcher and internals of all other apps. Using 'setu' you can directly open specific screens or actions of other apps without navigating all the way to it from the start. Use 'setu' to list all setus. Use 'setu new' to create a new Setu (Bridge). Use 'setu setu_name' to open a Setu for editing or deleting. Also, 'setu -1' destroys all your setus."
+    )
+
+    override fun execute(command: String) {
+        val args = command.split(" ")
+
+    }
+}
