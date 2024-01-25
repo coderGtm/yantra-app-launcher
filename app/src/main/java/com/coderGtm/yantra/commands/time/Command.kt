be1c77d7b9c6624600c8212c8640f5fbbd9c27e3
Anready <anready939@gmail.com>
2024-01-25 16:59:53 +0200

    modified command 'time'

--- app/src/main/java/com/coderGtm/yantra/commands/time/Command.kt
+++ app/src/main/java/com/coderGtm/yantra/commands/time/Command.kt
@@ -10,16 +10,32 @@ import java.util.Locale
 class Command(terminal: Terminal) : BaseCommand(terminal) {
     override val metadata = CommandMetadata(
         name = "time",
-        helpTitle = "time",
-        description = "Shows current local Date and Time"
+        helpTitle = "time <utc> [GMT]",
+        description = "Shows current local Date and Time or current GMT Date and time witch you provide"
     )
 
     override fun execute(command: String) {
         val args = command.split(" ")
-        if (args.size > 1) {
-            output("'time' command does not take any parameters", terminal.theme.errorTextColor)
+
+        if (args.size >= 2 && args[1] != "utc") {
+            output("Invalid second argument, please provide a correct parameter", terminal.theme.errorTextColor)
+            return
+        } else if (args.size == 2) {
+            output(currentTimeWithOffset(0))
+            return
+        } else if (args.size >= 3) {
+            val time = command.substringAfter(args[1]).trim()
+
+            if (!isCorrectString(time)) {
+                output("Invalid GMT, please provide a correct parameter", terminal.theme.errorTextColor)
+                return
+            }
+
+            val timeInInt = getNormalTimeString(time)
+            output(currentTimeWithOffset(timeInInt[0], timeInInt[1]))
             return
         }
+
         val time = SimpleDateFormat("HH:mm:ss E d/M/y", Locale.getDefault()).format(Date())
         output(time)
     }
