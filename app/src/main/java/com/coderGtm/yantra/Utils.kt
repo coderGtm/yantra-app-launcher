fb71f77ad301d80ff08930c6b343839b7cd018ea
Anready <104269567+Anready@users.noreply.github.com>
2024-10-23 21:12:51 +0300

    Speed-up call (#240)

--- app/src/main/java/com/coderGtm/yantra/Utils.kt
+++ app/src/main/java/com/coderGtm/yantra/Utils.kt
@@ -35,6 +35,9 @@ import com.coderGtm.yantra.terminal.Terminal
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory
 import com.google.android.play.core.install.model.UpdateAvailability
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.json.JSONArray
 import java.io.File
 import java.io.FileNotFoundException
@@ -91,46 +94,42 @@ fun requestCmdInputFocusAndShowKeyboard(activity: Activity, binding: ActivityMai
 @SuppressLint("Range")
 fun contactsManager(terminal: Terminal, callingIntent: Boolean = false, callTo: String = ""): List<Contacts> {
     terminal.contactsFetched = false
-    var builder = ArrayList<Contacts>()
-    // keep a list of contact names and their phone numbers whose name matches for calling
+    val builder = ArrayList<Contacts>()
     val callingCandidates = ArrayList<String>()
 
     val resolver: ContentResolver = terminal.activity.contentResolver
     val cursor = resolver.query(
-        ContactsContract.Contacts.CONTENT_URI, null, null, null,
-        null)
+        ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+        arrayOf(
+            ContactsContract.CommonDataKinds.Phone.CONTACT_ID,
+            ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+            ContactsContract.CommonDataKinds.Phone.NUMBER
+        ),
+        null, null, null
+    )
 
-    if (cursor!!.count > 0) {
-        while (cursor.moveToNext()) {
-            val id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID))
-            val name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
-            val phoneNumber = (cursor.getString(
-                cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))).toInt()
+    cursor?.use {
+        if (it.count > 0) {
+            while (it.moveToNext()) {
+                val id = it.getString(it.getColumnIndex(ContactsContract.CommonDataKinds.Phone.CONTACT_ID))
+                val name = it.getString(it.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))
+                val phoneNumValue = it.getString(it.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
 
-            if (phoneNumber > 0) {
-                val cursorPhone = resolver.query(
-                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                    null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=?", arrayOf(id), null)
+                builder.add(Contacts(name, phoneNumValue))
+                terminal.contactNames.add(name)
 
-                if(cursorPhone!!.count > 0) {
-                    while (cursorPhone.moveToNext()) {
-                        val phoneNumValue = cursorPhone.getString(
-                            cursorPhone.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
-                        builder.add(Contacts(name,phoneNumValue))
-                        terminal.contactNames.add(name)
-                        val phoneNumValueStandardized  = phoneNumValue.filterNot { it.isWhitespace() }
-                        if (callingIntent && callTo == name.lowercase() && !callingCandidates.contains(phoneNumValueStandardized)) {
-                            callingCandidates.add(phoneNumValueStandardized)
-                        }
-                    }
+                val phoneNumValueStandardized = phoneNumValue.filterNot { it.isWhitespace() }
+
+                if (callingIntent && callTo == name.lowercase() && !callingCandidates.contains(phoneNumValueStandardized)) {
+                    callingCandidates.add(phoneNumValueStandardized)
                 }
-                cursorPhone.close()
             }
+        } else {
+            terminal.output(terminal.activity.getString(R.string.no_contacts_found), terminal.theme.errorTextColor, null)
         }
-    } else {
-        terminal.output(terminal.activity.getString(R.string.no_contacts_found), terminal.theme.errorTextColor, null)
     }
-    cursor.close()
+
+
     if (callingIntent) {
         if (callingCandidates.isEmpty()) {
             terminal.output(terminal.activity.getString(R.string.contact_not_found_attempt_number), terminal.theme.resultTextColor, null)
@@ -159,14 +158,15 @@ fun contactsManager(terminal: Terminal, callingIntent: Boolean = false, callTo: 
                         positiveButton = terminal.activity.getString(R.string.select),
                         negativeButton = terminal.activity.getString(R.string.cancel),
                         positiveAction = {
+                            val candidatesArray = callingCandidates.toTypedArray()
                             val dialog2 = MaterialAlertDialogBuilder(terminal.activity, R.style.Theme_AlertDialog)
                                 .setTitle(terminal.activity.getString(R.string.select_phone_number))
                                 .setCancelable(false)
-                                .setItems(callingCandidates.toTypedArray()) { dialogInterface2, i ->
+                                .setItems(candidatesArray) { dialogInterface2, i ->
                                     terminal.output(terminal.activity.getString(R.string.calling, callTo), terminal.theme.successTextColor, null)
                                     val intent = Intent(
                                         Intent.ACTION_CALL,
-                                        Uri.parse("tel:${Uri.encode(callingCandidates[i])}")
+                                        Uri.parse("tel:${Uri.encode(candidatesArray[i])}")
                                     )
                                     terminal.activity.startActivity(intent)
                                     dialogInterface2.dismiss()
