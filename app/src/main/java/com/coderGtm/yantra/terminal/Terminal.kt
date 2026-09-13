342f2440b3f22af2e0c5daf2b967b2f5167be687
justneonixie <79766539+JustNeoNixy@users.noreply.github.com>
2026-09-13 13:16:10 -0400

    Fixed double termux output

--- app/src/main/java/com/coderGtm/yantra/terminal/Terminal.kt
+++ app/src/main/java/com/coderGtm/yantra/terminal/Terminal.kt
@@ -76,6 +76,8 @@ class Terminal(
     private var cmdHistoryCursor = -1
     private var commandCache = mutableListOf<Map<String, BaseCommand>>()
 
+    private var isProcessingInput = false
+
     val theme = getCurrentTheme(activity, preferenceObject)
     val commands = getAvailableCommands(activity)
     var primarySuggestions: MutableList<Suggestion> = mutableListOf()
@@ -361,12 +363,15 @@ class Terminal(
         }
     }
     fun handleInput(input: String) {
+        if (isProcessingInput) return
+        isProcessingInput = true
         suggestionJob?.cancel()
         binding.suggestionsTab.removeAllViews()
         handleCommand(input)
         binding.cmdInput.setText("")
         if (hideKeyboardOnEnter) hideSoftKeyboard()
         goFullScreen()
+        isProcessingInput = false
     }
     private fun hideSoftKeyboard() {
         binding.hideKeyboard()
