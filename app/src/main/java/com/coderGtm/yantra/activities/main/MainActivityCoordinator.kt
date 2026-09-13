d25a08264f3504630ab0b5ad0869fd03d3b05b69
justneonixie <79766539+JustNeoNixy@users.noreply.github.com>
2026-09-13 10:11:25 -0400

    fixed init runs twice

--- app/src/main/java/com/coderGtm/yantra/activities/main/MainActivityCoordinator.kt
+++ app/src/main/java/com/coderGtm/yantra/activities/main/MainActivityCoordinator.kt
@@ -98,7 +98,8 @@ internal class MainActivityCoordinator(
     }
 
     fun onStart() {
-        if (terminal.initialized && isPro(activity)) {
+        if (terminal.initialized && isPro(activity) && !terminal.initTasksQueued) {
+            terminal.initTasksQueued = true
             Thread {
                 val initList = getInit(app.preferenceObject)
                 runInitTasks(initList, app.preferenceObject, terminal)
